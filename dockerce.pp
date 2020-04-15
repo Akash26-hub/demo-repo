f5ad9a799afe0a90e6a22e6d31aaeb65dfbfeec1
Ad013 <ad.ankit013@gmail.com>
2020-04-16 00:47:44 +0530

    Create dockerce.pp

---
+++ dockerce.pp
@@ -0,0 +1,13 @@
+apt::key { '9DC858229FC7DD38854AE2D88D81803C0EBFCD88':
+    source => 'https://download.docker.com/linux/ubuntu/gpg',
+  } ->
+  apt::source { 'docker-ce':
+    architecture => 'amd64',
+    location     => 'https://download.docker.com/linux/ubuntu',
+    repos        => 'stable',
+    release      => $::lsbdistcodename,
+  } ->
+  package { 'docker-ce':
+    ensure  => 'latest',
+    require => Exec['apt_update'],
+  }
